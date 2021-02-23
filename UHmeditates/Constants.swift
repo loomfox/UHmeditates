5017b38c489acce176253c4d39db0037f0d86aaa
Piya Malhan <piyamalhan@gmail.com>
2021-02-22 16:51:09 -0800

    User Interface Framework Update --loginviewcontroller --constants.swift --scene_delegate --main storyboard --launchscreen storyboard --homeviewcontroller

---
+++ UHmeditates/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  UHmeditates
+//
+//  Created by Piya Malhan on 2/18/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct Constants {
+    struct Storyboard{
+        static let homeViewController = "HomeVC"
+        //static let tabViewController = "tb"
+        
+        
+    }
+}
+
+
