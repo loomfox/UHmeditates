52b8a69650e4120f77dd8cf2a3924a7530c59343
loomfox <62951557+loomfox@users.noreply.github.com>
2021-02-28 09:47:24 -0600

    Add files via upload

---
+++ uhmapi/src/models/UserModel.py
@@ -0,0 +1,65 @@
+from marshmallow import fields, Schema
+import datetime
+from . import db
+from ..app import bcrypt
+
+class UserModel(db.Model):
+  """
+  User Model
+  """
+
+  # table name
+  __tablename__ = 'users'
+
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(128), nullable=False)
+  email = db.Column(db.String(128), unique=True, nullable=False)
+  password = db.Column(db.String(128), nullable=True)
+  created_at = db.Column(db.DateTime)
+  modified_at = db.Column(db.DateTime)
+
+  # class constructor
+  def __init__(self, data):
+    """
+    Class constructor
+    """
+    self.name = data.get('name')
+    self.email = data.get('email')
+    self.password = self.__generate_hash(data.get('password'))
+    self.created_at = datetime.datetime.utcnow()
+    self.modified_at = datetime.datetime.utcnow()
+
+  def save(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self, data):
+    for key, item in data.items():
+      setattr(self, key, item)
+      self.password = self.__generate_hash(value) # add this new line
+        setattr(self, key, item)
+    self.modified_at = datetime.datetime.utcnow()
+    db.session.commit()
+
+ # add this new method
+  def __generate_hash(self, password):
+    return bcrypt.generate_password_hash(password, rounds=10).decode("utf-8")
+  
+  # add this new method
+  def check_hash(self, password):
+    return bcrypt.check_password_hash(self.password, password)
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  @staticmethod
+  def get_all_users():
+    return UserModel.query.all()
+
+  @staticmethod
+  def get_one_user(id):
+    return UserModel.query.get(id)
+
+  
+  def __repr(self):
+    return '<id {}>'.format(self.id)
