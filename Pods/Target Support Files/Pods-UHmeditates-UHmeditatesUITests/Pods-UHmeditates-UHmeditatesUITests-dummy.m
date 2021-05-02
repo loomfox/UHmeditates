e17ca65a6a388f01df204ddfc198109a20caaba9
Chase Philip <jacesilverlight@gmail.com>
2021-05-01 22:11:24 -0500

    This is the complete upload from chase, we can now start branching.

---
+++ Pods/Target Support Files/Pods-UHmeditates-UHmeditatesUITests/Pods-UHmeditates-UHmeditatesUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UHmeditates_UHmeditatesUITests : NSObject
+@end
+@implementation PodsDummy_Pods_UHmeditates_UHmeditatesUITests
+@end
---
+++ Pods/Target Support Files/Pods-UHmeditatesTests/Pods-UHmeditatesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UHmeditatesTests : NSObject
+@end
+@implementation PodsDummy_Pods_UHmeditatesTests
+@end
