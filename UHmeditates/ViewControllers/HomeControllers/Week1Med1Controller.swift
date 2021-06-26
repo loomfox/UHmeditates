ff9a4a20518e88ab53a856ee4272da8cce95cd13
bsniped <52137202+bsniped@users.noreply.github.com>
2021-06-26 10:00:52 -0500

    Created 2 new structures (MeditationQuiz.swift and Question.swift), populated Week1Med1 view controller on main.storyboard, and added starting code to start sending user answers to firebase.

--- UHmeditates/ViewControllers/HomeControllers/Week1Med1Controller.swift
+++ UHmeditates/ViewControllers/HomeControllers/Week1Med1Controller.swift
@@ -6,26 +6,70 @@
 //
 
 import UIKit
+import Firebase
+
 
 class Week1Med1Controller: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var buttonOne: UIButton!
+    @IBOutlet weak var buttonTwo: UIButton!
+    @IBOutlet weak var buttonThree: UIButton!
+    @IBOutlet weak var buttonFour: UIButton!
+    @IBOutlet weak var buttonFive: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    
+    var quizBrain = MeditationQuiz()
+    var userAnswer = "String"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let db = Firestore.firestore()
+        
+        db.collection("ButtonNamedCollectionHere").addDocument(data: [String(format: "Test", quizBrain.questionNumber) : userAnswer])
     
-        // Do any additional setup after loading the view.
+// MARK: Code that only needs to be ran once here
+            updateUI()
+        
+    }
+
+
+// MARK: IBActions Go Here
+
+    @IBAction func answerPressed(_ sender: UIButton) {
+        userAnswer = sender.currentTitle!
+            if userAnswer != nil {
+                sender.backgroundColor = UIColor.cyan
+            }
+    }
+    
+    @IBAction func nextQuestion(_ sender: UIButton) {
+        
+        Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        print(userAnswer)
+        quizBrain.nextQuestion()
     }
     
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// MARK: Refreshes UI
+    @objc func updateUI(){
+        
+        questionLabel.text = "\(quizBrain.questionNumber + 1). " + quizBrain.getQuestionText()
+        buttonOne.setTitle("\(quizBrain.getAnswerText()[0])", for: .normal)
+        buttonTwo.setTitle("\(quizBrain.getAnswerText()[1])", for: .normal)
+        buttonThree.setTitle("\(quizBrain.getAnswerText()[2])", for: .normal)
+        buttonFour.setTitle("\(quizBrain.getAnswerText()[3])", for: .normal)
+        buttonFive.setTitle("\(quizBrain.getAnswerText()[4])", for: .normal)
+        progressBar.progress = quizBrain.getProgress()
+        buttonOne.backgroundColor = UIColor.clear
+        buttonTwo.backgroundColor = UIColor.clear
+        buttonThree.backgroundColor = UIColor.clear
+    
     }
-    */
+    
 
 }
