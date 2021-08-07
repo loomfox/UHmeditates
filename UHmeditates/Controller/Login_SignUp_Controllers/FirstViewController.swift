149e862e3b60db980a6140412b9e29885abb51bf
bsniped <52137202+bsniped@users.noreply.github.com>
2021-08-07 17:25:52 -0500

    Re-imported lost folders (profile, login, dashboard)

---
+++ UHmeditates/Controller/Login_SignUp_Controllers/FirstViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UHmeditates
+//
+//  Created by Chase Philip on 1/25/21.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    
+    @IBOutlet weak var signupButton: UIButton!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
